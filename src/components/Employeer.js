c79da8d3658626a7326474bde9a8456a5a8a2bc1
thaisbrt <thais_brt@hotmail.com>
2022-03-26 17:34:32 -0300

    iniciada funcao updateEmployee

--- src/components/Employeer.js
+++ src/components/Employeer.js
@@ -12,24 +12,38 @@ export default function Employeer() {
             return alert('Some input is incorrect')
         } else {
             ++idEmployee
-            setEmployeeList([...employeeList, {idEmployee, name, email, occupation}])
+            setEmployeeList([...employeeList, { idEmployee, name, email, occupation }])
             return alert('Registered Successfully')
         }
     }
 
     function deleteEmployee(id) {
         setEmployeeList(employeeList.filter(emp => emp.idEmployee !== id))
-        console.log(employeeList)
         return alert('Deleted.')
     }
 
     function resetInput() {
         document.getElementById('form').reset();
     }
-    
-    function updateEmployee() {
-        let nameEmpl = name;
-        console.log(nameEmpl)
+
+    function updateEmployee(id) {
+        let inputName = document.getElementById('input-name')
+        inputName.value = name
+
+        let inputEmail = document.getElementById('input-email')
+        inputEmail.value = email
+
+        let inputOccup = document.getElementById('input-occup')
+        inputOccup.value = occupation;
+
+        let buttonSubmit = document.getElementById('button-submit')
+        buttonSubmit.value = 'Update Employee'
+
+        // employeeList.forEach(emp => {
+        //     if(emp.idEmployee === id) {
+        //         employeeList.name = inputName.value
+        //     }
+        // })       
     }
 
     return (
@@ -37,10 +51,10 @@ export default function Employeer() {
             <div>
                 <h1>EMPLOYEES</h1>
                 <form id='form' onSubmit={AddEmployee} className={styles.employeerContainer}>
-                    <input type='text' className={styles.employeerInput} placeholder="Enter the employee's name" onChange={(e) => setName(e.target.value)}></input>
-                    <input type='email' className={styles.employeerInput} placeholder="Enter the employee's email" onChange={(e) => setEmail(e.target.value)}></input>
-                    <input type='text' className={styles.employeerInput} placeholder="Enter the employee's occupation" onChange={(e) => setOccupation(e.target.value)}></input>
-                    <input type='submit' onClick={() => resetInput()} value='Add employee' />
+                    <input type='text' id='input-name' className={styles.employeerInput} placeholder="Enter the employee's name" onChange={(e) => setName(e.target.value)}></input>
+                    <input type='email' id='input-email' className={styles.employeerInput} placeholder="Enter the employee's email" onChange={(e) => setEmail(e.target.value)}></input>
+                    <input type='text' id='input-occup' className={styles.employeerInput} placeholder="Enter the employee's occupation" onChange={(e) => setOccupation(e.target.value)}></input>
+                    <input type='submit' id='button-submit' onClick={() => resetInput()} value='Add employee' />
                 </form>
             </div>
             <div>
@@ -51,7 +65,7 @@ export default function Employeer() {
                             <p>Employee's email: {element.email}</p>
                             <p>Employee's occupation: {element.occupation}</p>
                             <div>
-                                <button onClick={() => updateEmployee()}>Update</button>
+                                <button onClick={() => updateEmployee(element.idEmployee)}>Update</button>
                                 <button onClick={() => deleteEmployee(element.idEmployee)}>Delete</button>
                             </div>
                         </li>
